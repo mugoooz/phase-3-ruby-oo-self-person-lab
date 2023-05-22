d0e053a8c8d5badb9bef82896325fd023cb72d69
Eric Mugo <mugoericpg20@gmail.com>
2023-05-22 17:07:17 +0300

    added bonus

--- lib/person.rb
+++ lib/person.rb
@@ -9,7 +9,34 @@ class Person
         @hygiene = 8
     end
     
-    
+    def bank_account=(value)
+        @bank_account = value
+    end
+
+    def limit_value(value, min, max)
+        if value > max
+            max
+        elsif value < min
+            min
+        else
+            value
+        end
+    end
+
+    end
+
+    def happiness=(value)
+        @happiness = limit_value(value, 0, 10)
+    end
+
+    def hygiene=(value)
+        @hygiene = limit_value(value, 0, 10)
+    end
+
+    private
+
+   
+
 
 end
 
