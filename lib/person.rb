8fcc293a284fab5cae1190ffcc6906abd43b7888
Eric Mugo <mugoericpg20@gmail.com>
2023-05-22 16:44:36 +0300

    Added initial values

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,15 @@
 class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        @name=name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+    
+    
 
 end
 
