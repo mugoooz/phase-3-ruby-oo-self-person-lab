26bb1f64e7fa7c9f679bb29ea7ff55eabcaf9499
Eric Mugo <mugoericpg20@gmail.com>
2023-05-22 17:09:55 +0300

    Completed lab

--- lib/person.rb
+++ lib/person.rb
@@ -33,7 +33,51 @@ class Person
         @hygiene = limit_value(value, 0, 10)
     end
 
-    private
+    def clean?
+        hygiene > 7
+    end
+
+    def happy?
+        happiness > 7
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        slef.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+    
+    def call_friend(friend)
+        self.happiness += 3
+        friend.hapiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        case topic
+        when "politics"
+          self.happiness -= 2
+          friend.happiness -= 2
+          "blah blah partisan blah lobbyist"
+        when "weather"
+          self.happiness += 1
+          friend.happiness += 1
+          "blah blah sun blah rain"
+        else
+          "blah blah blah blah blah"
+        end
+    end
+    
 
    
 
